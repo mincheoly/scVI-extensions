4d8534a4451339bf549620de3a321656c703346c
Min Cheol Kim <mincheoly@gmail.com>
2018-08-19 15:34:30 -0700

    supervised

--- scripts/visualize_cropseq.py
+++ scripts/visualize_cropseq.py
@@ -12,24 +12,33 @@ import matplotlib
 matplotlib.use('AGG')
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-from scvi.dataset import CropseqDataset
+import argparse
+import torch
 
 from scvi.metrics.clustering import entropy_batch_mixing, get_latent
 from scvi.models import VAE, SVAEC
 from scvi.inference import VariationalInference
 
-import torch
+from scvi_extensions.dataset.cropseq import CropseqDataset
+from scvi_extensions.dataset.supervised_data_loader import SupervisedTrainTestDataLoaders
 
 if __name__ == '__main__':
 
+	# Argparse
+	parser = argparse.ArgumentParser(description='Visulaizing the latent space of scVI')
+	parser.add_argument('--model', type=str, metavar='M', help='path to the model to be used')
+	parser.add_argument('--data', type=str, metavar='D', help='path to the h5 data file')
+	parser.add_argument('--metadata', type=str,metavar='E', help='path to the tab separated metadata file')
+	parser.add_argument('--output', type=str, metavar='O', help='output file name')
+	args = parser.parse_args()
+
 	# Load the model
-	model_path = '/netapp/home/mincheol/vae_model_test.model'
+	model_path = args.model
 	vae = torch.load(model_path, map_location=lambda storage, loc: storage)
 
 	# Load the dataset
-	h5_filename = '/netapp/home/mincheol/raw_gene_bc_matrices_h5.h5'
-	metadata_filename = '/ye/yelabstore2/dosageTF/tfko_140/combined/nsnp20.raw.sng.km_vb1.norm.meta.txt'
+	h5_filename = args.data
+	metadata_filename = args.metadata
 	gene_dataset = CropseqDataset(
 		filename=h5_filename,
 		metadata_filename=metadata_filename,
@@ -37,37 +46,40 @@ if __name__ == '__main__':
 		use_labels=True,
 		save_path='')
 
-	# Create an inference class
-	use_batches=True
-	use_cuda=True
+	# Data Loader
+	n_samples = 10000
+	data_loader = SupervisedTrainTestDataLoaders(gene_dataset, num_samples=n_samples*10)
+
+	# Infer
+	n_epochs=500
+	lr=1e-3
+	use_batches=False
+	use_cuda=False
+
 	infer = VariationalInference(
 		vae, 
 		gene_dataset, 
 		train_size=0.9, 
 		use_cuda=use_cuda,
-		verbose=True,
 		frequency=5)
 
 	# Get the latent space
-	n_samples = 5000
-	latent, batch_indices, labels = get_latent(vae, infer.data_loaders['sequential'])
-	print(type(labels))
-	print(type(latent))
-	print(len(labels))
+	latent, batch_indices, labels = get_latent(vae, data_loader['all'])
 	latent, idx_t_sne = infer.apply_t_sne(latent, n_samples)
 	batch_indices = batch_indices[idx_t_sne].ravel()
 	labels = labels[idx_t_sne].ravel()
 
 	# Plot
 	plt.figure(figsize=(10, 10))
-	for i, guide in enumerate(np.unique(gene_dataset.labels)):
-		plt.scatter(latent[labels == guide, 0], latent[labels == guide, 1], label=guide)
+	ko_genes = np.array([gene_dataset[label] for label in labels], dtype=str)
+	for gene in np.unique(ko_genes):
+		plt.scatter(latent[ko_genes == gene, 0], latent[ko_genes == gene, 1], label=gene)
 	plt.title('tSNE plot of the scVI latent space')
 	plt.xlabel('tSNE1')
 	plt.ylabel('tSNE2')
 
 	# Save the plot
-	plt.savefig('/netapp/home/mincheol/plots/scvi_tsne.png')
+	plt.savefig(args.output)
 
 
 
