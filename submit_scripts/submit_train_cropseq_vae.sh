7cee65428f2f55716c4e1bcfeedef98ba5afcd13
mincheoly <mincheoly@gmail.com>
2018-08-21 08:03:55 -0700

    change num GPU

--- submit_scripts/submit_train_cropseq_vae.sh
+++ submit_scripts/submit_train_cropseq_vae.sh
@@ -6,7 +6,7 @@
 #$ -cwd                          
 #$ -r y                          
 #$ -j y                          
-#$ -l mem_free=50G                
+#$ -l mem_free=35G                
 #$ -l arch=linux-x64             
 #$ -l netapp=5G,scratch=5G      
 #$ -l h_rt=15:00:00
