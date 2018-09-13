efcb8771db8f042eeab67235bb30f0f8f44fa9da
Min Cheol Kim <mincheoly@gmail.com>
2018-09-13 11:39:26 -0700

    louvain

---
+++ scripts/cropseq_visualization.py
@@ -0,0 +1,55 @@
+"""
+	tsne_comparison.py
+	Visualize the latent space of the CROP-seq experiment.
+	Quantify the visualizations.
+"""
+
+
+import os
+
+import numpy as np
+import itertools
+from sklearn.manifold import TSNE
+import matplotlib
+matplotlib.use('AGG')
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import argparse
+import scanpy.api as sc
+
+
+if __name__ == '__main__':
+
+	# Argparse
+	parser = argparse.ArgumentParser(description='Clustering and visualizing latent space of scVI.')
+	parser.add_argument('--latent_space', type=str, help='path to the csv latent space file')
+	parser.add_argument('--n_neighbors', type=int, help='n_neighbors for SCANPY neighborhood calcuation')
+	parser.add_argument('--output', type=str, metavar='O', help='where the output files should go')
+
+	args = parser.parse_args()
+
+	# Load the CSV file into AnnData
+	adata = sc.read_csv(args.latent_space)
+
+	print('Using n_neighbors:', args.n_neighbors)
+
+	# Compute neighborhood
+	sc.pp.neighbors(adata, n_neighbors=args.n_neighbors, n_pcs=0, use_rep=None)
+
+	# Compute louvain
+	sc.tl.louvain(adata)
+	print('Number of louvain clusters:', len(adata.obs['louvain'].value_counts()))
+	adata.write(args.output + '/anndata_with_louvain_{}.h5ad'.format(args.n_neighbors))
+
+	# Compute, show, and save UMAP
+	sc.tl.umap(adata)
+	sc.pl.umap(adata, color='louvain');
+	plt.savefig(args.output + '/umap_louvain_{}.png'.format(args.n_neighbors))
+	plt.close()
+
+	# Compute, show, and save tSNE
+	sc.tl.tsne(adata, n_pcs=0, use_rep=None)
+	sc.pl.tsne(adata, color='louvain')
+	plt.savefig(args.output + '/tsne_louvain_{}.png'.format(args.n_neighbors))
+	plt.close()
---
+++ submit_scripts/submit_cropseq_visualization.sh
@@ -0,0 +1,41 @@
+#!/bin/bash                        
+#                           
+#$ -S /bin/bash                  
+#$ -o /netapp/home/mincheol/outputs
+#$ -e /netapp/home/mincheol/outputs
+#$ -cwd                          
+#$ -r y                          
+#$ -j y                          
+#$ -l mem_free=30G
+#$ -l arch=linux-x64             
+#$ -l netapp=5G,scratch=5G      
+#$ -l h_rt=20:00:00
+#$ -t 1-7                    
+
+# If you used the -t option above, this same script will be run for each task,
+# but with $SGE_TASK_ID set to a different value each time (1-10 in this case).
+# The commands below are one way to select a different input (PDB codes in
+# this example) for each task.  Note that the bash arrays are indexed from 0,
+# while task IDs start at 1, so the first entry in the tasks array variable
+# is simply a placeholder
+
+n_neighbor_array=(10 12 15 17 20 22 25)
+n_neighbor="${n_neighbor_array[$SGE_TASK_ID]}"
+
+export PYTHONPATH=/netapp/home/mincheol/scVI:/netapp/home/mincheol/scVI-extensions
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/netapp/home/mincheol/anaconda3/lib
+
+export CUDA_PATH=/ye/yelabstore2/mincheol/cuda-8.0
+export CUDA_HOME=$CUDA_PATH
+export LD_LIBRARY_PATH=$CUDA_PATH/lib64:$LD_LIBRARY_PATH
+export PATH=$PATH:/ye/yelabstore2/mincheol/cuda-8.0/bin
+
+source activate scvi
+python /netapp/home/mincheol/scVI-extensions/scripts/cropseq_visualization.py \
+	--latent_space /ye/yelabstore2/mincheol/scvi_extensions/scvi_latent_space.csv \
+	--n_neighbors $n_neighbor \
+	--output /ye/yelabstore2/mincheol/outputs/scvi/scvi_louvain
+source deactivate
+
+qstat -j $JOB_ID                                  # This is useful for debugging and usage purposes,
+                                                  # e.g. "did my job exceed its memory request?"
